5166eaadecbc3ec82dad4bb77572209b746bfc25
Nabil Adouani <nabil.adouani@gmail.com>
2018-06-06 16:24:29 +0200

    WIP: initial commit of jobs controller

--- cortex4py/controller/jobs.py
+++ cortex4py/controller/jobs.py
@@ -4,3 +4,15 @@ from .abstract import AbstractController
 class JobsController(AbstractController):
     def __init__(self, api):
         AbstractController.__init__(self, 'job', api)
+
+    def get_report(self, job_id):
+        return self._api.do_get('job/{}/report'.format(job_id)).json()
+
+    def get_report_async(self, job_id, duration='Inf'):
+        return self._api.do_get('job/{}/waitreport?atMost={}'.format(job_id, duration)).json()
+
+    def get_artifacts(self, job_id):
+        return self._api.do_get('job/{}/artifacts'.format(job_id)).json()
+
+    def revoke_key(self, job_id):
+        return self._api.do_delete('job/{}'.format(job_id))
